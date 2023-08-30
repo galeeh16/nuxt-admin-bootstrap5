6539cf88068702130d7d68d848d566921b00ec61
Galih Anggoro Jati <anggorogalih8@gmail.com>
2023-08-30 13:13:55 +0700

    fix ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
         {
             name: 'Admin_Bootstrap5',
             port: '3310',
-            // exec_mode: 'cluster', // ini modenya membuat beberapa aplikasi (load balancer)
-            exec_mode: 'node_cluster',
+            exec_mode: 'cluster', // ini modenya membuat beberapa aplikasi (load balancer)
+            // exec_mode: 'node_cluster',
             instances: 'max',
             script: './.output/server/index.mjs', // ini biarin aja (hasil build nuxtnya diexec)
         }
